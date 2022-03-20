a862dfd49ff347156046ec2405c805eeda8a3009
limchaekyung <corud505@naver.com>
2022-03-20 23:55:50 +0900

    offers, answers

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -5,32 +5,51 @@ const muteBtn = document.getElementById("mute")
 const cameraBtn = document.getElementById("camera")
 const camerasSelect = document.getElementById("cameras")
 
+const call = document.getElementById("call");
+
+call.hidden = true;
+
 let myStream;
 let muted = false;
 let cameraOff = false;
-
+let roomName;
+let myPeerConnection;
 
 async function getCameras() {
     try {
         const devices = await navigator.mediaDevices.enumerateDevices();
         const cameras = devices.filter(device => device.kind === "videoinput")
+        const currentCamera = myStream.getVideoTracks()[0]
         cameras.forEach(camera => {
             const option = document.createElement("option")
             option.value = camera.deviceId
             option.innerText = camera.label
+            if (currentCamera.label === camera.label) {
+                option.selected = true;
+            }
             camerasSelect.appendChild(option)
         })
     } catch (e) {
         console.log(e)
     }
 }
-async function getMedia() {
+async function getMedia(deviceId) {
+    const initialConstrains = {
+        audio: true,
+        video: { facingMode: "user" }
+    }
+    const cameraConstrains = {
+        audio: true,
+        video: { deviceId: { exact: deviceId } }
+    }
     try {
-        myStream = await navigator.mediaDevices.getUserMedia({
-            audio: true,
-            video: true,
-        })
+        myStream = await navigator.mediaDevices.getUserMedia(
+            deviceId ? cameraConstrains : initialConstrains
+        )
         myFace.srcObject = myStream;
+        if (!deviceId) {
+            await getCameras();
+        }
         await getCameras();
     } catch (e) {
         console.log(e)
@@ -54,7 +73,7 @@ function handleMuteClick() {
 
 function handleCameraClick() {
     myStream
-        .getvideoTracks()
+        .getVideoTracks()
         .forEach((track) => (track.enabled = !track.enabled));
     if (!cameraOff) {
         cameraBtn.innerText = "Turn Camera Off"
@@ -65,5 +84,58 @@ function handleCameraClick() {
     }
 }
 
+async function handleCameraChange() {
+    await getMedia(camerasSelect.value)
+}
 muteBtn.addEventListener("click", handleMuteClick)
 cameraBtn.addEventListener("click", handleCameraClick)
+camerasSelect.addEventListener("input", handleCameraChange)
+
+
+/* Welcome Form(join a room) */
+
+const welcome = document.getElementById("welcome");
+const welcomeForm = welcome.querySelector("form")
+
+async function initCall() {
+    welcome.hidden = true;
+    call.hidden = false;
+    await getMedia();
+    makeConnection()
+}
+
+async function handleWelcomeSubmit(e) {
+    e.preventDefault();
+    const input = welcomeForm.querySelector("input")
+    await initCall();
+    socket.emit("join_room", input.value)
+    roomName = input.value
+    input.value = ""
+}
+welcomeForm.addEventListener("submit", handleWelcomeSubmit)
+
+/* Socket Code */
+
+socket.on("welcome", async () => {
+    const offer = await myPeerConnection.createOffer();
+    myPeerConnection.setLocalDescription(offer)
+    console.log("sent the offer")
+    socket.emit("offer", offer, roomName)
+})
+
+socket.on("offer", async (offer) => {
+    myPeerConnection.setRemoteDescription(offer)
+    const answer = await myPeerConnection.createAnswer()
+    myPeerConnection.setLocalDescription(answer)
+    socket.emit("answer", answer)
+})
+
+socket.on("answer", answer => {
+    myPeerConnection.setLocalDescription(answer)
+})
+
+/* RTC Code */
+function makeConnection() {
+    myPeerConnection = new RTCPeerConnection()
+    myStream.getTracks().forEach(track => myPeerConnection.addTrack(track, myStream))
+}
--- src/server.js
+++ src/server.js
@@ -18,91 +18,20 @@ app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 /* 어떤 url로 이동하던지 /으로 이동 */
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
-
 const httpServer = http.createServer(app);
-const wsServer = SocketIo(httpServer, {
-    cors: {
-        origin: ["https://admin.socket.io"],
-        credentials: true,
-    }
-});
-instrument(wsServer, {
-    auth: false,
+const wsServer = SocketIo(httpServer);
+
+wsServer.on("connection", socket => {
+    socket.on("join_room", (roomName) => {
+        socket.join(roomName)
+        socket.to(roomName).emit("welcome")
+    })
+    socket.on("offer", (offer, roomName) => {
+        socket.to(roomName).emit("offer", offer)
+    })
+    socket.on("answer", (answer, roomName) => {
+        socket.to(roomName).emit("answer", answer)
+    })
 })
-
-function publicRooms() {
-    const {
-        sockets: {
-            adapter: { sids, rooms },
-        },
-    } = wsServer;
-    const publicRooms = [];
-    rooms.forEach((_, key) => {
-        if (sids.get(key) === undefined) {
-            publicRooms.push(key);
-        }
-    });
-    return publicRooms;
-}
-
-function countRoom(roomName) {
-    return wsServer.sockets.adapter.rooms.get(roomName)?.size;
-}
-
-wsServer.on("connection", (socket) => {
-    socket["nickname"] = "Anon";
-    socket.onAny((event) => {
-        console.log(`Socket Event: ${event}`);
-    });
-    socket.on("enter_room", (roomName, done) => {
-        socket.join(roomName);
-        done();
-        socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
-        wsServer.sockets.emit("room_change", publicRooms());
-    });
-    socket.on("disconnecting", () => {
-        socket.rooms.forEach((room) =>
-            socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)
-        );
-    });
-    socket.on("disconnect", () => {
-        wsServer.sockets.emit("room_change", publicRooms());
-    });
-    socket.on("new_message", (msg, room, done) => {
-        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
-        done();
-    });
-    socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
-});
-
-/* const wss = new WebSocket.Server({ server }); */
-/* 같은 서버에서 http, websocket 작동(필수 사항 아님) */
-/* express.js를 이용해서 http 서버 생성 */
-/* http 서버 위에 WebSocket 생성 */
-
-/* const sockets = [];
-
-wss.on("connection", (socket) => {
-    // socket: 연결된 브라우저 
-    sockets.push(socket);
-    socket["nickname"] = "Anon"; */    /* 익명 */
-/* console.log("Connected to Browser ✔")  //브라우저 켜졌을 때
-socket.on("close", () => { */              //브라우저 꺼졌을 때
-// 익명함수: 이름이 없는 function
-/* console.log("Disconnected from the Browser ❌")
-})
-socket.on("message", (msg) => { //브라우저가 서버에 메세지를 보낼 때 
-const message = JSON.parse(msg); */
-/* JSON.stringify: JavaScript object를 string으로 바꿔줌 */
-/* switch(message.type){
-    case "new_message":
-        sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`));
-    case "nickname":
-        console.log(message.payload)
-        socket["nickname"] = message.payload;
-}
-})
-}) */
-
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
 httpServer.listen(3000, handleListen);
